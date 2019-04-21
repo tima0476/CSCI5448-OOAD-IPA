1c890ca10df2d824675e8b3987537149aef7958f
Timothy Mason <Timothy.Mason@Colorado.edu>
2019-04-21 12:07:17 -0600

    Move to aggregation model for main window
    Instead of deriving the main program object from tkInter Frame, have the main frame be a separate object with a reference saved in the main program object (aggregation).
    
    Also began work on modularizing image opening functions.

--- IPA/IPA.py
+++ IPA/IPA.py
@@ -4,48 +4,66 @@
 
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
 
-imgdir = "/Users/tim/Pictures/"
-filename1 = 'Evelyn Smashed.jpg'
-filename2 = 'Evelyn Smashed.gif'
+from PIL import Image
+from PIL.ImageTk import PhotoImage 
+
+imgdir = "/Users/tim/Google Drive/Spring 2019/csci5448/Project/github/CSCI5448-OOAD-IPA/test_images/"
+filename2 = 'Evelyn Smash Burger.jpg'
+filename1 = '2008 Colder Bolder-55 smaller.jpg'
 
 
-class IPA(ttk.Frame):
-	"""This is the main program class of IPA: Image Processing Application"""
-	def __init__(self, parent=None, **options):
-		ttk.Frame.__init__(self, parent=None, **options)
-		self.pack()
-		self.master.title("IPA: Image Processing Application")
+class IPA:
+	"""
+	This is the main program class of IPA: Image Processing Application. 
+	"""
+	def __init__(self):
+		self.mainFrame = ttk.Frame(parent=None)
+		self.mainFrame.pack()
+		self.mainFrame.master.title("IPA: Image Processing Application")
 
-		self.img1 = Image.open(imgdir + filename1)
-		self.photoimg1 = ImageTk.PhotoImage(self.img1)
+		self.pilImages = []
+		self.tkImages = []
+		# self.imageTabs = []
 
-		self.img2 = Image.open(imgdir + filename2)
-		self.photoimg2 = ImageTk.PhotoImage(self.img2)
-		
-		self.nb = ttk.Notebook(self, padding=0)
-		self.frame1 = ttk.Frame(self.nb)
-		self.frame2 = ttk.Frame(self.nb)
-		self.nb.add(self.frame1, text='Image 1')
-		self.nb.add(self.frame2, text='Image 2')
+		self.nb = ttk.Notebook(self.mainFrame, padding=0)
 		self.nb.pack()
 
-		self.can = tk.Canvas(self.frame1)
-		self.can.config(width=self.photoimg1.width(), height=self.photoimg1.height())
-		self.can.create_image(2, 2, image=self.photoimg1, anchor=tk.NW)
-		self.can.pack(fill=tk.BOTH)
+		ttk.Button(self.mainFrame, text="Close").pack(side=tk.RIGHT)
+		ttk.Button(self.mainFrame, text="Save...").pack(side=tk.RIGHT)
+		ttk.Button(self.mainFrame, text='Open...', command=self.openButton).pack(side=tk.RIGHT)
 
-		self.can = tk.Canvas(self.frame2)
-		self.can.config(width=self.photoimg2.width(), height=self.photoimg2.height())
-		self.can.create_image(2, 2, image=self.photoimg2, anchor=tk.NW)
-		self.can.pack(fill=tk.BOTH)
+	def go(self):
+		self.mainFrame.mainloop()
+
+	def openButton(self):
+		print("Pressed Open...")
+		self.openImage(imgdir, filename1)
+
+	def openImage(self, path, filename):
+		print("openImage({},{})".format(path,filename))
+		# Open the image file using PIL and save a reference to the image object (aggregation)
+		pilImage = Image.open(path + filename)
+		tkImage = PhotoImage(image=pilImage)
+
+		# self.pilImages.append(pilImage)
+		self.tkImages.append(tkImage)	# must keep a reference so it doesn't get garbage collected.
+
+		# Add a tab to the main frame's notebook object, and display the image on that tab pane
+		frame = ttk.Frame(self.nb)
+		# self.imageTabs.append(frame)
+		self.nb.add(frame, text=filename)
+		# self.nb.pack()			# todo: necessary?
+
+		canvas = tk.Canvas(frame)
+		canvas.config(width=tkImage.width(), height=tkImage.height())	# To do - Add scrolling instead of forcing size
+		print("width", tkImage.width(), "height", tkImage.height() )
+		canvas.create_image(0, 0, image=tkImage, anchor=tk.NW)				 			# 0,0 is the relative coordinates of the image
+		canvas.pack(fill=tk.BOTH)
 
 
-		ttk.Button(self, text="Close").pack(side=tk.RIGHT)
-		ttk.Button(self, text="Save...").pack(side=tk.RIGHT)
-		ttk.Button(self, text='Open...').pack(side=tk.RIGHT)
 
 
 if __name__ == '__main__':
-	IPA().mainloop()
+	ipa = IPA()
+	ipa.go()			# This call doesn't return until the application terminates
