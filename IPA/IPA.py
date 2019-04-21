55c5ecb4245a228e264e5c683d48fe11d1a97846
Timothy Mason <Timothy.Mason@Colorado.edu>
2019-04-21 14:11:37 -0600

    fleshed out a tiny bit more of the button handlers

--- IPA/IPA.py
+++ IPA/IPA.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-# based from example tkinter code in "Programming Python, 4ed" by Mark Lutz
-
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image
@@ -9,8 +7,8 @@ from PIL.ImageTk import PhotoImage
 from ScrolledCanvas import ScrolledCanvas
 
 imgdir = "/Users/tim/Google Drive/Spring 2019/csci5448/Project/github/CSCI5448-OOAD-IPA/test_images/"
-filename1 = '2008 Colder Bolder-55.jpg'
-filename2 = 'Evelyn Smash Burger.jpg'
+filename2 = '2008 Colder Bolder-55.jpg'
+filename1 = 'Evelyn Smash Burger.jpg'
 
 class IPA:
 	"""
@@ -26,9 +24,9 @@ class IPA:
 		self.nb = ttk.Notebook(self.mainFrame, padding=0) # height and width are for size of initial blank Notebook
 		self.nb.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
 
-		ttk.Button(self.mainFrame, text="Close").pack(side=tk.RIGHT)
-		ttk.Button(self.mainFrame, text="Save...").pack(side=tk.RIGHT)
-		ttk.Button(self.mainFrame, text='Open...', command=self.openButton).pack(side=tk.RIGHT)
+		ttk.Button(self.mainFrame, text="Close", command=self.onCloseButtonPress).pack(side=tk.RIGHT)
+		ttk.Button(self.mainFrame, text="Save...", command=self.onSaveButtonPress).pack(side=tk.RIGHT)
+		ttk.Button(self.mainFrame, text='Open...', command=self.onOpenButtonPress).pack(side=tk.RIGHT)
 
 		# To Do if time:  Can I improve the packing of the widgets so the buttons don't collapse away on size-down?
 
@@ -38,15 +36,23 @@ class IPA:
 		"""
 		self.mainFrame.mainloop()
 
-	def openButton(self):
-		print("Pressed Open...")
+	def onCloseButtonPress(self):
+		print("onCloseButtonPress()")
+		# To Do - offer to save all unsaved edits
+		self.mainFrame.quit()
+
+	def onSaveButtonPress(self):
+		print("onSaveButtonPress()")
+
+	def onOpenButtonPress(self):
+		print("onOpenButtonPress()")
 		self.openImage(imgdir, filename1)
 
 	def openImage(self, path, filename):
 		"""
 		Given a path name, open the image file and add a tab to display it.
 		"""
-		print("openImage({},{})".format(path,filename))
+
 		# Open the image file using PIL and save a reference to the image object (aggregation)
 		pilImage = Image.open(path + filename)
 		tkImage = PhotoImage(image=pilImage)
@@ -72,6 +78,5 @@ class IPA:
 
 
 
-if __name__ == '__main__':
-	ipa = IPA()
-	ipa.go()			# This call doesn't return until the application terminates
+ipa = IPA()
+ipa.go()			# This call doesn't return until the application terminates
--- IPA/ScrolledCanvas.py
+++ IPA/ScrolledCanvas.py
@@ -1,14 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 
-
 class ScrolledCanvas(tk.Canvas):
     """
-    a canvas in a container that automatically makes
-    vertical and horizontal scroll bars for itself
-
-    This class is adapted from example code in “Programming Python , Fourth Edition", by Mark Lutz
-	(O’Reilly). Copyright 2011 Mark Lutz, 978-0-596-15810-1.
+    A tkinter canvas object that automatically makes scroll bars for itself
     """
     def __init__(self, container):
         tk.Canvas.__init__(self, container)
@@ -16,12 +11,14 @@ class ScrolledCanvas(tk.Canvas):
         vbar = tk.Scrollbar(container)
         hbar = tk.Scrollbar(container, orient='horizontal')
 
-        vbar.pack(side=tk.RIGHT,  fill=tk.Y)                 # pack canvas after bars
-        hbar.pack(side=tk.BOTTOM, fill=tk.X)                 # so clipped first
+        vbar.pack(side=tk.RIGHT,  fill=tk.Y)                 
+        hbar.pack(side=tk.BOTTOM, fill=tk.X)                 
         self.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES)
 
-        vbar.config(command=self.yview)                # call on scroll move
+        # The tkinter model has both the scrollbar and the canvas hooking into each other for event handling to make
+        # the scrollbars work properly
+        vbar.config(command=self.yview)
         hbar.config(command=self.xview)
-        self.config(yscrollcommand=vbar.set)           # call on canvas move
+        self.config(yscrollcommand=vbar.set)
         self.config(xscrollcommand=hbar.set)
 
