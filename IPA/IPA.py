5235ae831dc780189463f28d3e55f51ac132da8a
Timothy Mason <Timothy.Mason@Colorado.edu>
2019-04-21 22:39:58 -0600

    Zoom slider working (more or less)

--- IPA/IPA.py
+++ IPA/IPA.py
@@ -19,15 +19,42 @@ class IPA:
 		self.mainFrame.pack()
 		self.mainFrame.master.title("IPA: Image Processing Application")
 
-		self.tkImages = []		# for aggregation of tkInter image objects
+		# Initialize persistent storage lists for images.  To Do - Code Smell...Make an object storing all info about an object (MVC?)
+		self.pilImages = []		# store PIL image objects
+		self.tkImages = []		# store original tkInter PhotoImage objects
+		self.modTkImages = []	# store interim PhotoImage objects
+		self.imgCanvases = []	# store tkInter canvas objects (to enable changing the displayed image)
+		self.origSizes = []		# store (w,h) tuples with the original dimensions of each image
+		self.currZoom = []		# store the current zoom level for each image
 		
-		self.nb = ttk.Notebook(self.mainFrame, padding=0) # height and width are for size of initial blank Notebook
-		self.nb.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
+		# Create some frames to help achieve the desired layout of widgets.
+		imgFrame = tk.Frame(self.mainFrame)
+		imgFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
+		subFrame = tk.Frame(imgFrame)
+		subFrame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.TRUE)
 
+		# Add a slider (tk calls these "Scale") for the zoom control
+		self.zoomScale = ttk.Scale(subFrame, command=self.onZoomMove, from_=5, to=200, value=100, orient=tk.HORIZONTAL)
+		self.zoomScale.pack(side=tk.RIGHT, fill=tk.BOTH, expand=tk.TRUE)
+		ttk.Label(subFrame, text='Zoom: ').pack(side=tk.LEFT)	# Add a label so the user knows what it is
+
+		# add a ttk::Notebook widget.  This is a 'tabbed' panel where the images will be displayed...one image per 'tab'
+		self.nb = ttk.Notebook(imgFrame, padding=0)
+		self.nb.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.TRUE)
+		self.nb.bind("<<NotebookTabChanged>>", self.onTabChanged)	# IPA::onTabChanged will now be called whenever the selected tab changes.
+		self.nb.enable_traversal()	# enable keyboard controls.  
+			# From https://docs.python.org/2/library/ttk.html#ttk.Notebook ...
+			# This will extend the bindings for the toplevel window containing the notebook as follows:
+			#
+			# * Control-Tab: selects the tab following the currently selected one.
+			# * Shift-Control-Tab: selects the tab preceding the currently selected one.
+
+		# Create the row of control buttons along the bottom of the window in order from right to left
 		self.closeButton = tk.Button(self.mainFrame, text="Close", command=self.onCloseButtonPress)
 		self.saveButton = tk.Button(self.mainFrame, text="Save...", command=self.onSaveButtonPress, state='disabled')
 		self.openButton = tk.Button(self.mainFrame, text='Open...', command=self.onOpenButtonPress)
 
+		# place the buttons
 		self.closeButton.pack(side=tk.RIGHT)
 		self.saveButton.pack(side=tk.RIGHT)
 		self.openButton.pack(side=tk.RIGHT)
@@ -44,7 +71,6 @@ class IPA:
 		"""
 		Button handler:  Called when the Close button is pressed
 		"""
-		print("onCloseButtonPress()")
 		# To Do - offer to save all unsaved edits
 		self.mainFrame.quit()
 
@@ -52,7 +78,6 @@ class IPA:
 		"""
 		Button handler:  Called when the Save... button is pressed
 		"""
-		print("onSaveButtonPress()")
 
 	def onOpenButtonPress(self):
 		"""
@@ -60,7 +85,6 @@ class IPA:
 		"""
 		# Display a file chooser
 		filepath = filedialog.askopenfilename(initialdir = imgdir, title="Choose An Image to Open")
-		print("onOpenButtonPress() chose",filepath)
 
 		# Open the chosen file (if any)
 		if filepath:
@@ -72,30 +96,105 @@ class IPA:
 		"""
 
 		# Open the image file using PIL and save a reference to the image object (aggregation)
-		tkImage = PhotoImage(image=Image.open(filepath))
+		pilImage = Image.open(filepath)
+		tkImage = PhotoImage(image=pilImage)
+		origDim = (tkImage.width(), tkImage.height())
 
-		self.tkImages.append(tkImage)	# must keep a persistent reference so the image doesn't get garbage collected.
+		# Save the image information
+		self.pilImages.append(pilImage)
+		self.tkImages.append(tkImage)
+		self.modTkImages.append(tkImage)
+		self.origSizes.append(origDim)
+		self.currZoom.append(100)		# default zoom is 100%
 
 		# Add a tab to the main frame's notebook object, and display the image on that tab pane
 		(_,filename) = os.path.split(filepath)	# pull out the filename to use as the tab title
 		frame = tk.Frame(self.nb)
-		self.nb.add(frame, text=filename)
+		self.nb.add(frame, text=filename, sticky='nesw')
 
 		canvas = ScrolledCanvas(frame)
+		self.imgCanvases.append(canvas)
+
+		self.updateCanvasImage(canvas, tkImage)
+
+		# Now we have at least one image open.  Enable the Save... button
+		self.saveButton['state'] = 'normal'
+
+		# Make the new tab active
+		endIdx = self.nb.index("end")
+		self.nb.select(endIdx-1)
+
+	def updateCanvasImage(self, canvas, newImage):
+		"""
+		Given an existing tkInter canvas object and a PIL.PhotoImage, replace the image in the canvas
+		"""
 
 		# compute a maximum size based on the display size, leaving some buffer for other UI elements
 		screenW = self.mainFrame.winfo_screenwidth()-40
 		screenH = self.mainFrame.winfo_screenheight()-150
 
+		width = newImage.width()
+		height = newImage.height()
+
 		# create the canvas object on which the image will be displayed
-		canvas.config(width=min(screenW,tkImage.width()), height=min(screenH,tkImage.height()))	# size of the image region on screen
-		canvas.config(scrollregion=(0, 0, tkImage.width(), tkImage.height()))					# virtual size of the image
-		canvas.create_image(0, 0, image=tkImage, anchor=tk.NW)				 					# 0,0 is the relative coordinates of the image
+		canvas.config(width=min(screenW, width), height=min(screenH, height))	# size of the image region on screen
+		canvas.config(scrollregion=(0, 0, width, height))						# virtual size of the image
+		canvas.create_image(0, 0, image=newImage, anchor=tk.NW)				 			# 0,0 is the relative coordinates of the image
 		canvas.pack(fill=tk.BOTH)
 
-		# Now we have at least one image open.  Enable the Save... button
-		self.saveButton['state'] = 'normal'
+		# To Do:  Restructure this according to
+		# https://stackoverflow.com/questions/19838972/how-to-update-an-image-on-a-canvas
+		# (only call create_image on the first round, and itemconfig on subsequent
+		# updates.  Need to persist the object returned from the first
+		# create_image() call)
 
+
+	def zoomImage(self, value):
+		# Get the active tab #.  Abort if no tabs
+		try:
+			tabIdx = self.getCurrentTabID()
+		except:
+			return
+
+		newSize = ( int(self.origSizes[tabIdx][0]*float(value)/100.0), int(self.origSizes[tabIdx][1]*float(value)/100.0) )
+
+		newImg = self.pilImages[tabIdx].resize(size=newSize, resample=Image.LANCZOS)
+		newTkImg = PhotoImage(image=newImg)
+		self.modTkImages[tabIdx] = newTkImg
+
+		self.updateCanvasImage(self.imgCanvases[tabIdx], newTkImg)
+
+	def onZoomMove(self, value):
+		"""
+		Event handler - the Zoom slider has moved. Scale the image on the current tab accordingly.
+		"""
+		# Get the active tab #.  Abort if no tabs
+		try:
+			tabIdx = self.getCurrentTabID()
+		except:
+			return
+
+		# remember the new zoom for this image
+		self.currZoom[tabIdx] = int(round(float(value)))
+
+		# scale the image
+		self.zoomImage(value)
+		
+
+	def onTabChanged(self, event):
+		"""
+		Event handler - called whenever the main Notebook's active tab selection changes.
+		"""
+		tabIdx = self.getCurrentTabID()
+
+		# Update the zoom slider to the current zoom for this tab
+		self.zoomScale.set(self.currZoom[tabIdx])
+
+	def getCurrentTabID(self):
+		"""
+		Return the zero-based index of the currently selected tab in the main Notebook widget
+		"""
+		return self.nb.index( self.nb.select() )
 
 ipa = IPA()
 ipa.go()			# This call doesn't return until the application terminates
